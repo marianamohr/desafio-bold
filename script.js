da63239d946f545ed33802c74f36fb8646076dfc
Mariana Mohr <marianamohr@gmail.com>
2021-04-02 17:30:54 -0300

    Add arquivos ao repositorio

---
+++ script.js
@@ -0,0 +1,92 @@
+{
+    "products": [{
+        "id": 0,
+        "name": "Product Number 0",
+        "image": "//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6624363",
+        "oldPrice": 171,
+        "price": 140,
+        "description": "Product long description number 0 just to make more than one like of text.",
+        "installments": {
+            "count": 10,
+            "value": 14
+        }
+    }, {
+        "id": 1,
+        "name": "Product Number 1",
+        "image": "//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6829158",
+        "oldPrice": 295,
+        "price": 264,
+        "description": "Product long description number 1 just to make more than one like of text.",
+        "installments": {
+            "count": 10,
+            "value": 26.4
+        }
+    }, {
+        "id": 2,
+        "name": "Product Number 2",
+        "image": "//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6124117",
+        "oldPrice": 435,
+        "price": 391,
+        "description": "Product long description number 2 just to make more than one like of text.",
+        "installments": {
+            "count": 10,
+            "value": 39.1
+        }
+    }, {
+        "id": 3,
+        "name": "Product Number 3",
+        "image": "//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6124117",
+        "oldPrice": 215,
+        "price": 191,
+        "description": "Product long description number 3 just to make more than one like of text.",
+        "installments": {
+            "count": 10,
+            "value": 19.1
+        }
+    }, {
+        "id": 4,
+        "name": "Product Number 4",
+        "image": "//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=5783178",
+        "oldPrice": 283,
+        "price": 248,
+        "description": "Product long description number 4 just to make more than one like of text.",
+        "installments": {
+            "count": 10,
+            "value": 24.8
+        }
+    }, {
+        "id": 5,
+        "name": "Product Number 5",
+        "image": "//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6506376",
+        "oldPrice": 103,
+        "price": 58,
+        "description": "Product long description number 5 just to make more than one like of text.",
+        "installments": {
+            "count": 10,
+            "value": 5.8
+        }
+    }, {
+        "id": 6,
+        "name": "Product Number 6",
+        "image": "//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6584703",
+        "oldPrice": 234,
+        "price": 193,
+        "description": "Product long description number 6 just to make more than one like of text.",
+        "installments": {
+            "count": 10,
+            "value": 19.3
+        }
+    }, {
+        "id": 7,
+        "name": "Product Number 7",
+        "image": "//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=5783178",
+        "oldPrice": 280,
+        "price": 252,
+        "description": "Product long description number 7 just to make more than one like of text.",
+        "installments": {
+            "count": 10,
+            "value": 25.2
+        }
+    }],
+    "nextPage": "frontend-intern-challenge-api.iurykrieger.now.sh/products?page=2"
+}
